ee428c93ce22dbc09fc1a5e67ece6a4a4a0cab09
shehan <shehandj.swd@gmail.com>
2024-01-13 13:36:10 +0530

    Add firebase storage to the project

--- src/main/java/com/shehanrathnayake/AppInitializer.java
+++ src/main/java/com/shehanrathnayake/AppInitializer.java
@@ -1,15 +1,41 @@
 package com.shehanrathnayake;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import com.google.cloud.storage.Bucket;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class AppInitializer {
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public Bucket defaultBucket(@Value("librarian-9c5fe.appspot.com") String storageBucket) throws IOException {
+        InputStream serviceAccount =
+                new ClassPathResource("/firebase.json").getInputStream();
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket(storageBucket)
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) FirebaseApp.initializeApp(options);
+        return StorageClient.getInstance().bucket();
     }
 
     public static void main(String[] args) {
