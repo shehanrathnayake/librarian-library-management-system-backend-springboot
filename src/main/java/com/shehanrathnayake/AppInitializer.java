5b3ca297f98ffcd9c0c9024d49d0040a641914b7
shehan <shehandj.swd@gmail.com>
2024-01-13 13:21:22 +0530

    Initialize the project

---
+++ src/main/java/com/shehanrathnayake/AppInitializer.java
@@ -0,0 +1,18 @@
+package com.shehanrathnayake;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AppInitializer {
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppInitializer.class);
+    }
+}
